3064ced08ca05fdc924f8c5e686b2612f0cd9fed
manu <manuellatourf@gmail.com>
2025-12-02 21:56:34 +0100

    v2!

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
-// URL del backend (Render). Cambia aquí o usa localStorage:
-// localStorage.setItem('mp_api','https://gridchat.onrender.com')
-const API_BASE = "https://projectchat-yo8v.onrender.com/";
+const API_BASE_RAW =
+  window.localStorage.getItem("mp_api") ||
+  window.__API_BASE ||
+  "https://projectchat-yo8v.onrender.com";
+
+const API_BASE = String(API_BASE_RAW || "").replace(/\/+$/, "");
